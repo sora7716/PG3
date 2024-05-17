501f3a32b8e66b7434f4792d65f046f3be0b5a35
sora <147121795+sora7716@users.noreply.github.com>
2024-05-17 16:56:11 +0900

    テキストファイルの読み込み 読み込んだ文字のソート

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,44 @@
 ﻿#include <iostream>
-#include <cstdio>
 #include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
 using namespace std;
 
+vector<string>ParseCustomASCII(const string& filePath) {
+	ifstream file(filePath);//ファイルの読み込み
+	vector<string>data;//読み込んだデータを格納する可変長配列
+	string line;//ファイルから読み込んだ1行を格納する変数
+
+	//開けなかったとき
+	if (!file.is_open()) {
+		cerr << "Unable to open file;" << filePath << endl;
+		return data;//空のdataを返す
+	}
+	//一行ずつずつ読み込む
+	while (getline(file, line)) {
+		stringstream lineStream(line);//読み込んだ行を文字列ストリームに変換
+		string cell;//行から切り出した単語を格納する変数
+
+		while (getline(lineStream, cell, ',')) {//lineStreamで行を読み込みカンマ区切りで単語をcellに格納する
+			data.push_back(cell);//カンマごとに区切られた単語をrow格納
+		}
+	}
+	return data;//パースしたデータを返す
+}
+
 int main() {
-	
-	cout << "あああ" << endl;
+
+	string filePath = "PG3_03_02.txt";//読み込むファイルのパス
+	auto data = ParseCustomASCII(filePath);//ファイルをパースしてデータを取得
+	//データのソート
+	sort(data.begin(), data.end());
+	//パースシテデータを出力
+	for (const auto &row : data) {
+        // 文字列の先頭と末尾の "[" と "]" を削除して出力
+        cout << row.substr(1, row.size() - 2) << endl;
+    }
+
 	return 0;
 }
