8de20111443077792c9440820dfc02b6d620eb27
sora <147121795+sora7716@users.noreply.github.com>
2024-05-17 14:31:17 +0900

    数字を昇順と降順に並び替えた

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -5,17 +5,25 @@
 using namespace std;
 
 int main() {
+	//int型の動的配列a(要素:5)
 	vector<int>a = { -10,321,-3,50 };
 
-	cout << "全要素を表示します" << endl;
-
+	cout << "全要素を表示します:ソート前" << endl;
+	//全要素の表示
 	for (int i = 0; i < a.size(); i++) {
 		cout << a[i] << endl;
 	}
 
-	sort(a.begin(), a.end());
-	cout << "全要素を表示します" << endl;
-	
+	sort(a.begin(), a.end());//昇順に並び変える
+	cout << "全要素を表示します:昇順" << endl;
+	//全要素の表示:昇順
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i] << endl;
+	}
+
+	reverse(a.begin(), a.end());//降順に並び変える
+	cout << "全要素を表示します:降順" << endl;
+	//全要素の表示:降順
 	for (int i = 0; i < a.size(); i++) {
 		cout << a[i] << endl;
 	}
