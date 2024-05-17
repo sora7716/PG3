67528beaa69500c3dee5bbfa376e14a092ce2871
sora <147121795+sora7716@users.noreply.github.com>
2024-05-17 14:20:06 +0900

    整列させるプログラムを書いた

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
+﻿#include <iostream>
 #include <cstdio>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
-	
+	vector<int>a = { -10,321,-3,50 };
 
+	cout << "全要素を表示します" << endl;
+
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i] << endl;
+	}
+
+	sort(a.begin(), a.end());
+	cout << "全要素を表示します" << endl;
+	
+	for (int i = 0; i < a.size(); i++) {
+		cout << a[i] << endl;
+	}
 	return 0;
 }
