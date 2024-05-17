a21ffa0d81da7cee0bffbdb0287ce5173989be70
sora <147121795+sora7716@users.noreply.github.com>
2024-05-17 14:43:32 +0900

    trueの数を数えるプログラム

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -3,8 +3,9 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-	
-	cout << "あああ" << endl;
-	return 0;
+int main(int argc,char const*argv[]) {
+	bool b_array[] = { true,false,true,true };
+	size_t n_count = count(b_array, b_array + (sizeof(b_array)), true);//trueの数を数える
+	cout << n_count << endl;
+    return 0;
 }
