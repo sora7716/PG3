c95d2234c43ab8be5bae821954dd28a37940fc45
sora <147121795+sora7716@users.noreply.github.com>
2024-05-17 14:37:26 +0900

    探索用のプログラム

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,19 @@
 ﻿#include <iostream>
 #include <cstdio>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
-	
-	cout << "あああ" << endl;
+	//動的配列
+	vector<int>a = { -10,321,-3,50,1 };
+	//探索したいvector用のイテレータ
+	vector<int>::iterator it;
+	//指定した値を探索
+	it = find(a.begin(), a.end(), -3);
+	//指定した値が存在すれば
+	if (it != a.end()) {
+		cout << *it << endl;
+	}
 	return 0;
 }
