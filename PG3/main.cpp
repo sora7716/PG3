8eca6a4e333a469d26849fd5ad13e11d411150c3
sora <147121795+sora7716@users.noreply.github.com>
2024-05-17 15:03:13 +0900

    最後の関数をラムダ式に変更した

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -2,10 +2,16 @@
 #include <algorithm>
 using namespace std;
 
-void display(int i) { cout << i << endl; }
+
 
 int main(int argc,char const*argv[]) {
 	int i_array[] = { 1,2,3 };
-	for_each(begin(i_array), end(i_array),display);//最後に入れた関数を逐次実行してくれる
+	for_each(
+		begin(i_array), 
+		end(i_array), 
+		[](int& i) {
+		cout << i << endl;
+		}
+	);//最後に入れた関数を逐次実行してくれる
 	return 0;
 }
