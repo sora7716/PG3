7be7ffc52334742be008ed7cf6f5590c1b5c4cd2
sora <147121795+sora7716@users.noreply.github.com>
2024-05-17 15:01:32 +0900

    逐次実行してくれるアルゴリズム

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>
-#include <cstdio>
 #include <algorithm>
 using namespace std;
 
-int main() {
-	
-	cout << "あああ" << endl;
+void display(int i) { cout << i << endl; }
+
+int main(int argc,char const*argv[]) {
+	int i_array[] = { 1,2,3 };
+	for_each(begin(i_array), end(i_array),display);//最後に入れた関数を逐次実行してくれる
 	return 0;
 }
