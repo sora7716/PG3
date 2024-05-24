c89d35e81633c5cb5ed562a0a8ab2444f92585ff
sora <147121795+sora7716@users.noreply.github.com>
2024-05-24 15:38:44 +0900

    ClassNameクラスの作成

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -1,9 +1,13 @@
-﻿#include <iostream>
 #include <cstdio>
+#include <iostream>
+#include "ClassName.h"
 using namespace std;
 
 int main() {
-	cout << "Hello World" << endl;
+	ClassName<int> num1{2,3};
+	ClassName<float> num2{2.3f,3.9f};
 
+	cout << num1.add() << endl;
+	cout << num2.add() << endl;
 	return 0;
 }
